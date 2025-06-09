3aca5f5c0db5cab6bb2cc045a9e3c659da54025f
Batrana_KPI_Script <hppall@163.com>
2025-06-09 14:33:49 +0800

    Update GetYoutubeUrl.py
    update url  'yudou666.com'  to  'https://www.mibei77.com'

--- GetYoutubeUrl.py
+++ GetYoutubeUrl.py
@@ -2,12 +2,15 @@ from requests_html import HTMLSession
 from datetime import datetime
 
 
-def getYoutubeUrl():
+
+url = 'https://www.mibei77.com/'
+
+def getYoutubeUrl(url):
     session = HTMLSession()
     # 其中get请求中的参数和requests库中的get是一样的可以随意添加
 
     # 这里是获取yudou66的首页最新的一个分享文章的链接
-    yudouSession = session.get('https://www.yudou66.com/')
+    yudouSession = session.get(url)
 
     yudouAElement = yudouSession.html.xpath('//*[@id="Blog1"]/div[1]/article[1]/div[1]/h2/a')
     yudouTodayUrl = yudouAElement[0].attrs['href']
@@ -28,7 +31,7 @@ def getYoutubeUrl():
     return {'youtubeUrl' : youtubeUrl,'yudouTodayUrl': yudouTodayUrl}
 
 if __name__ == '__main__':
-    video_result =getYoutubeUrl()
+    video_result =getYoutubeUrl(url)
     print(video_result)
 
 
